d05c71cf9496d083b904f6caecd014d2a6916b23
pierslabs <pedrolosasp@gmail.com>
2022-01-17 11:21:01 +0100

    variable entorno

--- index.js
+++ index.js
@@ -35,6 +35,6 @@ const server = new ApolloServer({
 });
 
 // arrancar servidor
-server.listen().then(({ url }) => {
+server.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
   console.log(`servidor listo en la URL ${url}`);
 });
